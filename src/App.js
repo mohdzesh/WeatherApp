95e4b614b927115f4b0d7afa7be2015a585d125b
Mohammed Zeeshan <72436049+mohdzesh@users.noreply.github.com>
2023-11-30 14:07:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import Axios from "axios";
 import CityComponent from "./modules/CityComponent";
 import WeatherComponent from "./modules/WeatherInfoComponent";
